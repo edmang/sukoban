68526707d1579cb632102c722ab0563d3f278df9
eywang <edmond.yizhou.wang@gmail.com>
2021-05-17 01:26:29 +0200

    code clean

--- move.c
+++ move.c
@@ -8,6 +8,23 @@
 #include "coord.h"
 #include "material.h"
 
+bool isBox(Material* material, SDL_Surface*** map, int y, int x) {
+    return map[y][x] == material->box;
+}
+
+bool isBoxOk(Material* material, SDL_Surface*** map, int y, int x) {
+    return map[y][x] == material->boxOk;
+}
+
+bool isTarget(Material* material, SDL_Surface*** map, int y, int x) {
+    return map[y][x] == material->target;
+}
+
+bool isMoveOk(Material* material, SDL_Surface*** map, int y, int x) {
+    return !((map[y][x] == material->wall) || (map[y][x] == material->box) ||
+             (map[y][x] == material->boxOk));
+}
+
 void handleMoveOutFromTarget(Material* material, SDL_Surface*** map,
                              Coord nextNextCoord) {
     if (isTarget(material, map, nextNextCoord.y, nextNextCoord.x)) {
@@ -36,48 +53,6 @@ void actuallyMove(Material* material, SDL_Surface*** map, Mario* mario,
             map[nextCoord.y][nextCoord.x] = material->target;
             fun(material, map, mario, nextCoord);
         }
-    }
-}
-
-//
-bool isBox(Material* material, SDL_Surface*** map, int y, int x) {
-    return map[y][x] == material->box;
-}
-
-bool isBoxOk(Material* material, SDL_Surface*** map, int y, int x) {
-    return map[y][x] == material->boxOk;
-}
-
-bool isTarget(Material* material, SDL_Surface*** map, int y, int x) {
-    return map[y][x] == material->target;
-}
-
-bool isMoveOk(Material* material, SDL_Surface*** map, int y, int x) {
-    return !((map[y][x] == material->wall) || (map[y][x] == material->box) ||
-             (map[y][x] == material->boxOk));
-}
-
-void move(Material* material, SDL_Surface*** map, Direction direction,
-          Mario* mario) {
-    Coord nextCoord = getNextCoordFromPixel(direction, mario->curPos);
-    Coord nextNextCoord = getNextCoordFromCoord(direction, nextCoord);
-
-    switch (direction) {
-        case UP:
-            actuallyMove(material, map, mario, nextCoord, nextNextCoord, up);
-            break;
-
-        case DOWN:
-            actuallyMove(material, map, mario, nextCoord, nextNextCoord, down);
-            break;
-
-        case LEFT:
-            actuallyMove(material, map, mario, nextCoord, nextNextCoord, left);
-            break;
-
-        case RIGHT:
-            actuallyMove(material, map, mario, nextCoord, nextNextCoord, right);
-            break;
     }
 }
 
@@ -132,3 +107,29 @@ void right(Material* material, SDL_Surface*** map, Mario* mario, Coord coord) {
         }
     }
 }
+
+//
+void move(Material* material, SDL_Surface*** map, Direction direction,
+          Mario* mario) {
+    Coord nextCoord = getNextCoordFromPixel(direction, mario->curPos);
+    Coord nextNextCoord = getNextCoordFromCoord(direction, nextCoord);
+
+    switch (direction) {
+        case UP:
+            actuallyMove(material, map, mario, nextCoord, nextNextCoord, up);
+            break;
+
+        case DOWN:
+            actuallyMove(material, map, mario, nextCoord, nextNextCoord, down);
+            break;
+
+        case LEFT:
+            actuallyMove(material, map, mario, nextCoord, nextNextCoord, left);
+            break;
+
+        case RIGHT:
+            actuallyMove(material, map, mario, nextCoord, nextNextCoord, right);
+            break;
+    }
+}
+
--- move.h
+++ move.h
@@ -9,21 +9,4 @@
 
 void move(Material* material, SDL_Surface*** map, Direction direction,
           Mario* mario);
-
-void up(Material* material, SDL_Surface*** map, Mario* mario, Coord nextCoord);
-void down(Material* material, SDL_Surface*** map, Mario* mario,
-          Coord nextCoord);
-void left(Material* material, SDL_Surface*** map, Mario* mario,
-          Coord nextCoord);
-void right(Material* material, SDL_Surface*** map, Mario* mario,
-           Coord nextCoord);
-
-bool isBox(Material* material, SDL_Surface*** map, int y, int x);
-
-bool isBoxOk(Material* material, SDL_Surface*** map, int y, int x);
-
-bool isMoveOk(Material* material, SDL_Surface*** map, int y, int x);
-
-bool isTarget(Material* material, SDL_Surface*** map, int y, int x);
-
 #endif
